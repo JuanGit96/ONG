8e50159aeecf8a5473da9407050773509cdf80bf
Juan Cardenas <jdcardenasd10@gmail.com>
2019-03-04 22:00:42 -0500

    cambios front-end

--- resources/views/donations.blade.php
+++ resources/views/donations.blade.php
@@ -51,7 +51,7 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="{{url('/')}}"><b>adminlte-laravel</b></a>
+                <a class="navbar-brand" href="{{url('/')}}"><b>Findesin</b></a>
             </div>
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
